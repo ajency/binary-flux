d876ac369cb5591e8d91131ab5e535140612d9d1
Vaibhav Arolkar <vaibhav.a@ajency.in>
2019-12-17 15:48:44 +0530

    Fixes

--- docs/js/app.js
+++ docs/js/app.js
@@ -7,5 +7,15 @@ $(document).ready(function(){
             dropdownCssClass: "select-out-dark-result",
             allowClear: true
         });
-    }    
+    } 
+    
+    $('.side-menu-toggle').click(function () {
+        $('html,body').addClass('overflow-hidden');
+        $(".sidear-container").addClass('show');
+    });
+
+    $('.mobile-slide-in .close').click(function () {
+        $('html,body').removeClass('overflow-hidden');
+        $(".sidear-container").removeClass('show');
+    });
 });
