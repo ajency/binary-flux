b51dd477534877a203101bb8ff0fbae94f32477e
Ankita <ankita@ajency.in>
2020-05-26 19:36:17 +0530

    build

--- docs/js/app.js
+++ docs/js/app.js
@@ -66,6 +66,43 @@ $(document).ready(function(){
         nextArrow: '<button type="button" class="slick-next"></button>'
     });
     
+    $('.dashboard-slider').slick({
+        infinite: false,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: true,
+        cssEase: 'linear', 
+        prevArrow: '<button type="button" class="slick-prev"></button>',  
+        nextArrow: '<button type="button" class="slick-next"></button>',
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
     // particlesJS("particles-js", {
     //     "particles": {
     //         "number": {
