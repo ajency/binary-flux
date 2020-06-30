c00f655f9f5595fa510ca8d8789e26ef4d5ef786
Ankita <ankita@ajency.in>
2020-06-30 11:14:53 +0530

    transition delay

--- themes/binary/static/js/app.js
+++ themes/binary/static/js/app.js
@@ -71,14 +71,14 @@ $(document).ready(function(){
     $('.xs-sidebar').mouseenter(function(){
             // collapsed
             $(".sidebar").stop(true,true).css({width: '200px'});
-            $(".content").stop(true,true).css({paddingLeft: '210px',  transition: '0.2s ease-in'});
+            $(".content").stop(true,true).css({paddingLeft: '210px',  transition: '0.4s ease-in'});
             $(".small-sidebar").hide();
-            $(".hide-sidebar").fadeIn(1000);
+            $(".hide-sidebar").fadeIn(1800);
     });
     $('.xs-sidebar').mouseleave(function(){
             // collapsed
             $(".sidebar").stop(true,true).css({width: '50px'});
-            $(".content").stop(true,true).css({paddingLeft: '60px', transition: '0.2s ease-in'});
+            $(".content").stop(true,true).css({paddingLeft: '60px', transition: '0.4s ease-in'});
             $(".hide-sidebar").hide();
             $(".small-sidebar").show();
     });
